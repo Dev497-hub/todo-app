0508dc6f6db91a5ee21c2ee94386dbf363ac3233
Dev497-hub <developergit1986@outlook.com>
2025-02-18 12:15:32 +0530

    Create TodoController.java

---
+++ src/main/java/com/todo/controller/TodoController.java
@@ -0,0 +1,37 @@
+package com.todo.controller;
+
+import com.todo.model.Todo;
+import com.todo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodo(@PathVariable Long id, @RequestBody Todo todo) {
+        todo.setId(id);
+        return todoRepository.save(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id) {
+        todoRepository.deleteById(id);
+    }
+}
